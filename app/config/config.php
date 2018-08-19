92e768dac13189b47f3922be4e1c46f6d81ab7e1
iamaian <The21stKuruno@yahoo.com>
2018-08-20 07:07:29 +0900

    done creating mvc

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', '_YOUR_USER_');
+  define('DB_PASS', '_YOUR_PASS_');
+  define('DB_NAME', '_YOUR_DBNAME_');
+
+  // App Root
+  define('APPROOT', dirname(__FILE__, 2)) ;
+  // URL Root
+  define('URLROOT', '_YOUR_URL_');
+  // Site Namme
+  define('SITENAME', '_YOUR_SITENAME_');
